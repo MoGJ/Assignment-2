e01efdadf6d7de715d35402cf73fd20298af5c91
MoGJ <38267866+MoGJ@users.noreply.github.com>
2019-10-20 19:02:41 -0500

    Changes to Name Change
    Changes made to change name from BulletinSpace to Nava_Assignment2

---
+++ Nava_Assignment2/service/src/main/java/com/nava_maven_assignment_1/People.java
@@ -0,0 +1,69 @@
+package com.mycompany.nava_maven_assignment_1;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Collection;
+
+/**
+ *
+ * @author Johnathan Nava
+ */
+public class People {
+    private static String readURL(String urlString) throws Exception {
+        BufferedReader reader = null;
+        try {
+            URL url = new URL(urlString);
+            reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            StringBuilder buffer = new StringBuilder();
+            int read;
+            char[] chars = new char[1024];
+            while ((read = reader.read(chars)) != -1) {
+                buffer.append(chars, 0, read);
+            }
+            return buffer.toString();
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+        }
+
+    }
+
+    public void jsonPull() throws Exception {
+        String jsonOrg = readURL("https://www.w3schools.com/angular/customers.php");
+        String json = jsonOrg.replace("\"records\":", "");
+
+        System.out.println("json = " + json);
+
+        //Name, City, Country in double quotes, need to remove double quotes
+        String json1 = json.replaceFirst("\\{", "");
+        String json2 = json1.replace("\"Name\"", "Name");
+        String json3 = json2.replace("\"City\"", "City");
+        String json4 = json3.replace("\"Country\"", "Country");
+        String json5 = json4.substring(0, json4.length()-3);
+        
+        System.out.println("json = " + json5);
+
+        Gson gson = new Gson();
+
+        java.lang.reflect.Type collectionType = new TypeToken<Collection<Person>>() {
+        }.getType();
+        Collection<Person> enums = gson.fromJson(json5, collectionType);
+
+        System.out.println("test\n" +
+                "----Name" + "\t\t" + "  City" + "\t" + "  Country----");
+
+        for(Person person : enums) {
+            System.out.println(person.Name + "\t" + person.City +
+                    "\t" + person.Country);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        People peoples = new People();
+        peoples.jsonPull();
+    }
+}
---
+++ Nava_Assignment2/service/src/main/java/com/nava_maven_assignment_1/Person.java
@@ -0,0 +1,35 @@
+package com.mycompany.nava_maven_assignment_1;
+
+/**
+ *
+ * @author Johnathan Nava
+ */
+public class Person {
+    public String Name;
+    public String City;
+    public String Country;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String Country) {
+        this.Country = Country;
+    }
+}
